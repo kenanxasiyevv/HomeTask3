3e18b8b2fd3f85d4ac351fbce2631451d87e628a
kenanxasiyevv <kananakh@code.edu.az>
2022-12-28 03:45:48 +0400

    Add project files.

---
+++ HomeTask3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("eded daxil edin : ");
+            int i = int.Parse(Console.ReadLine());
+            {
+                if (i >= 10000 && i <= 99999)
+
+                    Console.WriteLine(i * 10 + 4000002);
+                else
+                    Console.WriteLine("eded 5 reqemli deyil");
+
+
+
+            }
+        }
+    }
+}
